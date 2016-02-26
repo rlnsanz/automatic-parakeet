7910b3fc039171562d8f05f5a70a6736b3c84a21
jkhutch3 <jkhutch3@asu.edu>
2016-02-26 13:43:28 -0800

    Made it so that spork.c could be compiled (does not nesscarily work). Made a makefile for spork.c. You guys will have to make it work for you system. This is being pushed to a non-master branch.

--- p2Module/spork.c
+++ p2Module/spork.c
@@ -19,7 +19,7 @@
 #define fork_rate_cap 100
 #define fork_abs_cap 16000
 
-extern void *sys_call_table[];
+//extern void *sys_call_table[];
 
 static struct task_struct *bash; 
 
@@ -28,27 +28,32 @@ int (*orig_sys_fork)  (void);
 
 
 asmlinkage long sys_spork(void) {
-	unsigned long tob = 0;
+	unsigned long tob = 0, tob_s = 0;
 	struct task_struct *task;
 	int tasks_this_sec = 0, nr_tasks = 0;
 	struct pid* pid;
 	if (current->pid == pid_vnr(task_session(current)) && current->pid > 1)
 		bash = current;
 	tob = jiffies;
-	read_lock(&tasklist_lock);
+	tob_s = jiffies/HZ;
+	//read_lock(&tasklist_lock);
 	// Critical Section
 	for_each_process(task) {
 		nr_tasks++;
-		if (60*(tob - usecs_to_jiffies((unsigned int)(current->start_time/1000))) < 1 ) {
+		/*// This is too long it throws an "__udivdi3" undefined error
+		if (60*(tob - usecs_to_jiffies((unsigned long long)(current->start_time/1000))) < 1 ) {
 			tasks_this_sec++;
-		}
+		}*/
+		
+		if(tob_s - usecs_to_jiffies((unsigned long)current->start_time/1000) < 60)
+			tasks_this_sec++;
 		// get the thread leader
 		pid = get_task_pid(task, PIDTYPE_SID);
 		
 	}
-	read_unlock(&tasklist_lock);
+	//read_unlock(&tasklist_lock);
 	if (tasks_this_sec > fork_rate_cap) {
-		read_lock(&tasklist_lock);
+		//read_lock(&tasklist_lock);
 		for_each_process(task) { 
 			rcu_read_lock();
 			if (pid_vnr(task_session(current)) == pid_vnr(task_session(bash)) && task != bash) {
@@ -56,7 +61,7 @@ asmlinkage long sys_spork(void) {
 			} 
 			rcu_read_unlock();
 		}
-		read_unlock(&tasklist_lock);
+		//read_unlock(&tasklist_lock);
 	}
 	
 	return (orig_sys_fork());
@@ -65,7 +70,7 @@ asmlinkage long sys_spork(void) {
 int init_module()
 {
 	//orig_sys_fork  = sys_call_table[__NR_fork];
-	SYSCALL_DEFINE2(sys_spork, long);
+	//SYSCALL_DEFINE2(sys_spork, long);
 	printk(KERN_INFO "Spork loaded\n");
 	return(0);
 }
@@ -73,6 +78,6 @@ int init_module()
 
 void cleanup_module()
 {
-	sys_call_table[__NR_fork]  = orig_sys_fork; 
+	//sys_call_table[__NR_fork]  = orig_sys_fork; 
 }
 
