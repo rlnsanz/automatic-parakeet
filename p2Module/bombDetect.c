2fdf0fca0fa72b9f31b251e4538494728598b7d5
jkhutch3 <jkhutch3@asu.edu>
2016-02-28 18:33:00 -0800

    So I added the while loop to help the code run and detect it. (It also prints and kills now to). It does not run indeffinatly right now. It does not run in the background, I think.

--- p2Module/bombDetect.c
+++ p2Module/bombDetect.c
@@ -4,7 +4,6 @@
 #include <linux/pid_namespace.h>
 
 // MACROS (arbitrary numbers)
-#define MAX_NUM_OF_PROG 350	// NEED TO DETERMINE (350 is my guess)
 #define MAX_NUM_NEW_PRO 5	// NEED TO DETERMINE (5 is my guess)
 
 /**
@@ -39,32 +38,33 @@ int detectBomb(void){
 	for_each_process(task) nr_tasks1++;
 
 	// Waits HZ jiffies (should be 1 second)
-	jif2 = wait_jiffies_from_start(jif1,HZ);
+	jif2 = wait_jiffies_from_start(jif1,10);
 	
 	// Iterates through each task and counts them (more tasks than last time?)
 	for_each_process(task) nr_tasks2++;
 	
 	// If either of the arbitrary maxes are met (print and kill the new tasks)
-	if(nr_tasks2 > MAX_NUM_OF_PROG || nr_tasks2-nr_tasks1 > MAX_NUM_NEW_PRO){
+	if(nr_tasks2-nr_tasks1 > MAX_NUM_NEW_PRO){
 		for_each_process(task){
 			if(++nr_tasks3 > nr_tasks1){
-			// Currently kills all tasks that were created since program start
-				printk("Task name is %s\nPID is %d\n", task->comm, task->pid);
+				printk("%d\n",task->pid);
 				send_sig_info(SIGKILL, SEND_SIG_FORCED, task);
 			}
 		}
+		printk("Tasks per 10 jiffies = %d\n", nr_tasks2-nr_tasks1);
 	}
-	//printk("Task name is %s\nPid is %d\n", task->comm, task->pid);
-	//printk("There are %d tasks\n",nr_tasks1);
-	//printk("There are %d tasks\n", nr_tasks2);
-	//printk("There have been %lu jiffies.\n", jif2-jif1);
 	
 	return 0;
 }
 
 
 int detect_bomb_init(void){
-	detectBomb();
+	// Remove the i < j and just make it 1 to run forever
+	int i = 0, j = 200;
+	while(i < j){
+		detectBomb();
+		i++;
+	}
 	return 0;
 }
 void detect_bomb_exit(void){
