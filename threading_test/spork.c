ba24ebd129fd2888574e821e37ead2464d215572
Daniel D'Souza <daniel.dsouza5@gmail.com>
2016-02-29 11:28:29 -0700

    a few changes to threading

--- threading_test/spork.c
+++ threading_test/spork.c
@@ -11,22 +11,36 @@
 #include <linux/sched.h>
 
 #include <linux/semaphore.h>
+#include <linux/delay.h>
 
-struct task_struct *monitor_task;
+struct task_struct *monitor_task, *kill_task;
 int monitor_function(void *data);
-int kill_forks(void);
+int kill_function(void *data);
 
 int monitor_function(void *data) {
-    printk(KERN_INFO "IN THREAD FUNCTION");
+    printk(KERN_INFO "MONITORING THREAD STARTED");
+    
+    // do not stop this thread until the program quits.
     while(!kthread_should_stop()) {
-        schedule();
+	// run basic detection task here (the count).
+	// if some condition
+        //     kill_task = kthread_create(&kill_function, (void *)data, "kill_task");
+        //     acutally just call the method, this should be easier...
+        
+        // this will put the process to sleep, and keep it that way...
+        // set_current_state(TASK_INTERRUPTIBLE);
+        // schedule();
 
+        msleep(10); //sleep for a 10th of a second.
     }
     return 0;
 }
 
-int kill_forks(void) {
+int kill_function(void *data) {
+    printk(KERN_INFO "KILLING THREAD STARTED");
 
+    // add detection and kill code here.
+    
     return 0;
 }
 
